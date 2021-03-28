daf4548c0424ee904bdde3956cf48d57605552ac
andrescomar <andrescomar@hotmail.com>
2021-03-28 18:07:34 -0300

    for

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+
+for (let index = 0; index < 10; index++) {
+    console.log("Estamos contando el numero es: " + index)    
+}
+
+let myContador = parseInt(prompt("Contar hasta donde"));
+for (let index = 0; index < myContador ; index++) {
+    
+    
+}
