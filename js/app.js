4a690defc01bf030358dfd13430990555618ba21
andrescomar <andrescomar@hotmail.com>
2021-03-28 20:10:13 -0300

    desafio clase 3

--- js/app.js
+++ js/app.js
@@ -1,33 +1,33 @@
 // FOR
 
-for (let index = 0; index <= 10; index++) {
-    console.log("Estamos contando el numero es: " + index)    
+for (let index = 10; index <= 50; index++) {
+    console.log("Número ingresado: " + index)    
 }
 
-for (let index = 0; index < 10; index=index+2) {
-    console.log("Estamos contando el numero es: " + index)    
+for (let index = 0; index < 20; index=index+4) {
+    console.log("Número ingresado: " + index)    
 }
 
 
-let myContador = parseInt(prompt("Contar hasta donde"));
-for (let index = 0; index <= myContador ; index++) {
-    console.log("Estamos contando el numero es: " + index);    
+let Contador = parseInt(prompt("Contar hasta el número:"));
+for (let index = 0; index <= Contador ; index++) {
+    console.log("Número ingresado: " + index);    
 }  
 
-for (let index = 0; ; index++) {
-    console.log("Estamos contando el numero es " + index);
-    if (index > 10) {
+for (let index =15; ; index++) {
+    console.log("Número ingresado " + index);
+    if (index > 30) {
         break;
     }
 }
 
 // WHILE
 
-let datosEntrada = prompt("ingrese datos(si ingresas ESC termina)")
+let numeroID = prompt("Ingrese su número de ID o Contraseña (Escriba Finalizar si desea terminar el proceso)")
 
-while( datosEntrada != "ESC"){
-    console.log("el usuario ingreso " + datosEntrada);
-    datosEntrada = prompt("ingrese datos(si ingresas ESC termina)");
+while( numeroID != "Finalizar"){
+    console.log("el usuario ingreso " + numeroID);
+    numeroID = prompt("Ingrese su número de ID o Contraseña (Escriba Finalizar si desea terminar el proceso)");
 }
 
 let numero = 0;
@@ -38,22 +38,21 @@ do {
 } while (parseInt(numero));
 
 // SWITCH 
-let SwitchDatos = prompt("ingrese datos(si ingresas ESC termina)");
+let NumeroID = prompt("Ingrese su número de ID o Contraseña (Escriba Finalizar si desea terminar el proceso)");
 
-while(SwitchDatos != "ESC"){
+while(NumeroID != "Finalizar"){
 
-    switch (SwitchDatos) {
-        case "Nintento":
-             console.log("Hola Nintento Switch")
+    switch (NumeroID) {
+        case "2021":
+             console.log("Número de ID es correcto")
             break;
-        case "Play":
-             console.log("Hola PlayStation")
+        case "CoderHouse":
+             console.log("La cotraseña ingresada es correcta")
         default:
-             console.log("No se permite Xbox")
+             console.log("ID o contraseña ingresada son incorrectas, no puede pasar")
             break;
     }
 
-
-    SwitchDatos = prompt("ingrese datos(si ingresas ESC termina)");
+    NumeroID = prompt("Ingrese su número de ID o Contraseña (Escriba Finalizar si desea terminar el proceso)");
 }
 
