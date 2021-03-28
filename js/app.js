17a9d3e901e4ca3dd47547886cc5175e7f678efe
andrescomar <andrescomar@hotmail.com>
2021-03-28 18:11:47 -0300

    Contadores

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 
 
-for (let index = 0; index < 10; index++) {
+for (let index = 0; index <= 10; index++) {
     console.log("Estamos contando el numero es: " + index)    
 }
 
 let myContador = parseInt(prompt("Contar hasta donde"));
-for (let index = 0; index < myContador ; index++) {
-    
-    
+for (let index = 0; index <= myContador ; index++) {
+    console.log("Estamos contando el numero es: " + index);    
+
 }
